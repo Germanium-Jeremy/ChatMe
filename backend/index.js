87a42d553ea7d7e2ad223e3cf67b3f4ba54772f2
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-01 18:03:06 +0200

    request modify

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Origin", "https://chat-me-6r1j.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://chat-me-6r1j.vercel.app/",
+    "http://localhost:5173",
+  ]);
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
