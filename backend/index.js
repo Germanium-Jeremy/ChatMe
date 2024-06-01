1fa5047399d06ec06ec0155d8a3ba61dba74859a
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-01 18:09:03 +0200

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,13 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", [
-    "https://chat-me-6r1j.vercel.app/",
-    "http://localhost:5173",
-  ]);
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+app.use(
+  cors({
+    origin: ["https://chat-me-6r1j.vercel.app", "http://localhost:5173"],
+    methods: "*",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use("/api", userRoute);
 app.use("/api", chatRoute);
 app.use("/api", messageRoute);
